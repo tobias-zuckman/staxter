177b8a1d60203c294af5b5d88e9b12c04f7dde83
tobias-zuckman <55133140+tobias-zuckman@users.noreply.github.com>
2019-09-10 11:55:59 +0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Staxter 555  !";
+        return "Hello from Staxter!";
     }
     
 }
