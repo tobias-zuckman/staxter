70715b118c1c6f0bb0eb3e0a4452b5e9f116e793
tobias-zuckman <55133140+tobias-zuckman@users.noreply.github.com>
2019-09-10 12:23:14 +0300

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/hello
 echo `sspwd`
-kill -9 `dfps -ef | grep  java | grep -v grep  | awk '{print $2}'`
+ttttkill -9 `dfps -ef | grep  java | grep -v grep  | awk '{print $2}'`
 dsffd 
 mvn gdfdfclean spring-boot:run > /dev/null 2>&1 &
 
